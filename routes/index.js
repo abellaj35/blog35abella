f701b2c1518896aab19bac2680f99d5835c48afd
abellaj35 <abellajuan2002@gmail.com>
2019-12-17 13:52:08 -0300

    agrege cosas

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/post');
+const Comment = require('../models/Comment');
+const mongoose = require('mongoose')
 
 router.get('/', (req, res) => {
   Post.find().exec((err, posts) => {
@@ -12,8 +14,43 @@ router.get('/newpost', (req, res ) => {
   res.render('newpost');
 });
 
-router.get('/post/:id', (req, res) => {
-  res.render('post');
+router.get('/posts/:id', (req, res) => {
+  Post.findById(req.params.id).populate('comments').exec((err, post) => {
+    res.render('post', { post: post });
+  });
 });
+
+router.get('/newpost', (req, res) => {
+  res.render('newpost');
+});
+
+router.post('/newcomment', (req, res) => {
+  const comment = new Comment({
+    _id: new mongoose.Types.ObjectId(),
+    author: req.body.author,
+    content: req.body.content
+  });
+  comment.save(err => {
+    Post.findById(req.body.postId).exec((err, post) => {
+      post.comments.push(comment._id);
+      post.save(err => {
+        res.redirect('/posts/' + req.body.postId);
+      });
+    });
+  });
+});
+
+router.post('/newpost', (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    subtitle: req.body.subtitle,
+    author: req.body.author,
+    content: req.body.content
+  });
+  post.save(err => {
+    res.redirect('/');
+  });
+});
+
 
 module.exports = router;
